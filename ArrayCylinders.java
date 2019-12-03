51244cdfd51b95f56328a51f1edfe993d5ce43f6
nichsr18 <nichsr18@wfu.edu>
2019-12-03 17:18:55 -0500

    Finished exercise(12/3)

--- ArrayCylinders.java
+++ ArrayCylinders.java
@@ -1,3 +1,4 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class ArrayCylinders {
@@ -6,9 +7,16 @@ public class ArrayCylinders {
        Scanner in = new Scanner(System.in);
        Cylinder list []= new Cylinder[5] ;
        //1) fill the information for each Cylinder from the user
+        for(int i=0;i<5;i++){
+            list[i]=new Cylinder(in.nextInt(),in.nextInt());
+
+        }
 
 
         // 2)print the information of all Cylinders
+        for(int i=0; i<5;i++) {
+            list[i].print();
+        }
 
 
     }
